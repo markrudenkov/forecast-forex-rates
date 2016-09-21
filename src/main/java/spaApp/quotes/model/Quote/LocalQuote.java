4131445356861c1a9a7c6dbbf741324702108bd9
markrudenkov <mark.rudenkov@gmail.com>
2016-09-22 02:21:51 +0300

    LocalQuote created; implemented mapToQuoteDB and mapToLocalQuote in QuoteService

---
+++ src/main/java/spaApp/quotes/model/Quote/LocalQuote.java
@@ -0,0 +1,97 @@
+package spaApp.quotes.model.Quote;
+
+import org.joda.time.DateTime;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by mark on 16.9.22.
+ */
+public class LocalQuote {
+
+    private Long id;
+    private String symbol;
+    private DateTime date;
+    private BigDecimal open;
+    private BigDecimal high;
+    private BigDecimal low;
+    private BigDecimal close;
+    private BigDecimal adjustedClose;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public BigDecimal getOpen() {
+        return open;
+    }
+
+    public void setOpen(BigDecimal open) {
+        this.open = open;
+    }
+
+    public BigDecimal getHigh() {
+        return high;
+    }
+
+    public void setHigh(BigDecimal high) {
+        this.high = high;
+    }
+
+    public BigDecimal getLow() {
+        return low;
+    }
+
+    public void setLow(BigDecimal low) {
+        this.low = low;
+    }
+
+    public BigDecimal getClose() {
+        return close;
+    }
+
+    public void setClose(BigDecimal close) {
+        this.close = close;
+    }
+
+    public BigDecimal getAdjustedClose() {
+        return adjustedClose;
+    }
+
+    public void setAdjustedClose(BigDecimal adjustedClose) {
+        this.adjustedClose = adjustedClose;
+    }
+
+    public DateTime getDate() {
+        return date;
+    }
+
+    public void setDate(DateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "symbol='" + symbol + '\'' +
+                ", date=" + date +
+                ", open=" + open +
+                ", high=" + high +
+                ", low=" + low +
+                ", close=" + close +
+                ", adjustedClose=" + adjustedClose +
+                '}';
+    }
+}
