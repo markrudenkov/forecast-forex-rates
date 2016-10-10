f0a4a1aad7c2defb08735ab091ce005302481e8e
markrudenkov <mark.rudenkov@gmail.com>
2016-10-11 01:03:13 +0300

    fixed bugs of table displaying

--- src/main/app/quote/components/quote.js
+++ src/main/app/quote/components/quote.js
@@ -4,7 +4,7 @@ function Controller ($scope, QuoteService){
    var vm = this;
    symbolAndDate={};
    vm.symbolAndDate=symbolAndDate;
-   vm.quotes={};
+   vm.quotes=[];
    vm.errors={};
    vm.currencyPairs=["EUR%3dX","JPY=X","GBPUSD=X","AUDUSD=X","NZDUSD=X","EURJPY=X","GBPJPY=X","EURGBP=X","EURCAD=X","EURCHF=X"];
    vm.getQuotes=getQuotes;
@@ -12,29 +12,28 @@ function Controller ($scope, QuoteService){
 
     vm.$onInit = function(){
            getQuotes();
-       }
+    }
 
     function getQuotes(){
-             symbolAndDate.symbol=$scope.radioModel;
-             symbolAndDate.startDate=$scope.startDate;
-             symbolAndDate.endDate = $scope.endDate;
+         symbolAndDate.symbol=$scope.radioModel;
+         symbolAndDate.startDate=$scope.startDate;
+         symbolAndDate.endDate = $scope.endDate;
 
-             QuoteService.getQuotes(symbolAndDate).then(
-                   function(response){
-                       vm.quotes = response.data;
-                       console.log('ItWorks');
-                       console.log(vm.quote);
-                   },
-                   function(err){
-                       if(err.status === 400){
-                           vm.errors=err.data;
-                       }else {
-                           console.log('Error',err);
-                       }
+         QuoteService.getQuotes(symbolAndDate).then(
+               function(response){
+                   vm.quotes = response.data;
+                   console.log('ItWorks');
+                   console.log(vm.quotes);
+               },
+               function(err){
+                   if(err.status === 400){
+                       vm.errors=err.data;
+                   }else {
+                       console.log('Error',err);
+                   }
 
-                   });
+               });
      }
-
 
     //Datepicker
 
@@ -48,52 +47,45 @@ function Controller ($scope, QuoteService){
 
        minDate: new Date(),
        showWeeks: true
-     };
+    };
 
-     $scope.dateOptions = {
+   $scope.dateOptions = {
        dateDisabled: disabled,
        formatYear: 'yy',
        maxDate: new Date(2020, 5, 22),
        minDate: new Date(),
        startingDay: 1
-     };
+    };
 
      // Disable weekend selection
-     function disabled(data) {
+   function disabled(data) {
        var date = data.date,
          mode = data.mode;
        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-     }
+   }
 
-     $scope.toggleMin = function() {
+   $scope.toggleMin = function() {
        $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
        $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
-     };
+   };
 
      $scope.toggleMin();
 
-
-     $scope.open1 = function() {
+   $scope.open1 = function() {
        $scope.popup1.opened = true;
-     };
+   };
 
-     $scope.open2 = function() {
+   $scope.open2 = function() {
        $scope.popup2.opened = true;
-     };
+   };
 
-     $scope.popup1 = {
+   $scope.popup1 = {
        opened: false
-     };
+   };
 
-     $scope.popup2 = {
+   $scope.popup2 = {
        opened: false
-     };
-
-
-
-
-
-
+   };
 }
 
 Controller.$inject = ['$scope','QuoteService'];
