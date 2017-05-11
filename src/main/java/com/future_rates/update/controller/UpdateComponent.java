552a7ff5851f09ac9b50f07b4acddc81b9d5d194
markrudenkov <mark.rudenkov@gmail.com>
2017-05-12 01:13:49 +0300

    app config

--- src/main/java/com/future_rates/update/controller/UpdateComponent.java
+++ src/main/java/com/future_rates/update/controller/UpdateComponent.java
@@ -20,7 +20,7 @@ public class UpdateComponent {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 600000)
     public void updateRates() {
         updateService.updateAllInstruments();
         log.info("Currency rates updated{}", dateFormat.format(new Date()));
